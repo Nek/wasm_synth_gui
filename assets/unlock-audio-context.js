5e9ed01fb1c660b2129323f69dc695dd97da1b8f
Nikita Dudnik <nikdudnik@gmail.com>
2022-09-27 13:14:16 +0200

    More clean up

--- assets/unlock-audio-context.js
+++
@@ -1,7 +0,0 @@
-export function unlockAudioContext(cb) {
-    const b = document.body;
-    const events = ["touchstart", "touchend", "mousedown", "keydown"];
-    events.forEach(e => b.addEventListener(e, unlock, false));
-    function unlock() {cb(); clean();}
-    function clean() {events.forEach(e => b.removeEventListener(e, unlock));}
-}
